8a071340fcc3d4f965393f0609e68285b16c8567
Steve <stevegardiner26@aol.com>
2019-11-09 21:34:00 -0500

    Added Routes/Views for Rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,12 @@
-class RoomController < ApplicationController
+class RoomsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:show]
 
   def index
+    @rooms = @current_user.rooms
+  end
+
+  def show
 
   end
 end
