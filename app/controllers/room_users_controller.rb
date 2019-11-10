f864338e2f435e79a76916a8e25991d0a30dc07e
Steve <stevegardiner26@aol.com>
2019-11-10 04:19:45 -0500

    Added Homepage Buttons

---
+++ app/controllers/room_users_controller.rb
@@ -0,0 +1,6 @@
+class RoomUsersController < ApplicationController
+
+  def new
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   resources :rooms
+  resources :room_users
 end
