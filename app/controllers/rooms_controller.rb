b5b24b2b219d47db6220ed848469934316f62d3a
Steve <stevegardiner26@aol.com>
2019-11-22 14:08:00 -0500

    Added More Room Controller Options

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -13,7 +13,12 @@ class RoomsController < ApplicationController
 
   def create
     @room = @current_user.rooms.create(owner_id: @current_user.id, name: params[:room][:name], starting_balance: params[:room][:starting_balance])
-    redirect_to @room
+    if @room.save
+      flash[:success] = "Room #{@room.name} was created successfully"
+      redirect_to @room
+    else
+      render :new
+    end
   end
 
   def show
@@ -23,7 +28,7 @@ class RoomsController < ApplicationController
   private
 
   def room_params
-    params.permit(:name, :starting_balance)
+    params[:room].permit(:name, :starting_balance)
   end
 
   def permission_check
