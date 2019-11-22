8c9b2d144b2a3c1a3bf8029e9feaf8a1ca8071b0
Steve <stevegardiner26@aol.com>
2019-11-22 11:59:23 -0500

    Added email validation on User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :email, uniqueness: true, presence: true
+
   has_many :room_users, dependent: :destroy
   has_many :rooms, through: :room_users
 end
